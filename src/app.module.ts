bd691c551ca0ff9033d20447b824174c7b86ad26
Miles Felton Gerber <mwfelton03@gmail.com>
2023-10-02 14:46:22 +0100

    connected to postgresql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // ConfigModule.forRoot(),
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      password: 'code',
+      username: 'postgres',
+      entities: [User],
+      database: 'ee_database',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
